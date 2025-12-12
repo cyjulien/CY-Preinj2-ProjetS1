4b43bfbb980e633b175127ecd1b80407f153cd25
cyjulien <julien.renard@etu.cyu.fr>
2025-12-12 17:50:39 +0100

    Add files via upload

---
+++ CODE/AVL.c
@@ -0,0 +1,36 @@
+typedef struct _treeNode{
+  int value;
+  struct _treeNode* right;
+  struct _treeNode* left;
+  int balanced;
+} TreeNode;
+
+TreeNode* makeTreeNode(int value) {
+  TreeNode* treeNode = NULL;
+  treeNode = malloc(sizeof(TreeNode));
+  if (treeNode == NULL) exit(1);
+  treeNode->right = NULL;
+  treeNode->left = NULL;
+  treeNode->value = value;
+  treeNode->balanced = 0;
+  return treeNode;
+}
+
+int hasLeftChild(TreeNode* treeNode) {
+  if (treeNode == NULL) return 0;
+  return treeNode->left != NULL;
+}
+int hasRightChild(TreeNode* treeNode) {
+  if (treeNode == NULL) return 0;
+  return treeNode->right != NULL;
+}
+void addLeftChild(TreeNode** treeNode, int n) {
+  if (*treeNode == NULL) return;
+  (*treeNode)->left = makeTreeNode(n);
+  //return (*treeNode)->left;
+}
+void addRightChild(TreeNode** treeNode, int n) {
+  if (*treeNode == NULL) return;
+  (*treeNode)->right = makeTreeNode(n);
+  //return (*treeNode)->right;
+}
---
+++ CODE/TreeGen.c
@@ -0,0 +1,49 @@
+#include "main.h"
+#include "AVL.h"
+
+typedef struct Section {
+  Section plantSrc;
+  char id[128];
+  int volumeIn;
+  float leaksOut;
+  struct Section** upstream;
+  struct Section** downstream;
+} Section;
+
+int main(int argc, char const *argv[]) {
+  char plantSrc[81];
+  char upstreamId[81];
+  char downstreamId[81];
+  char volume[81];
+  char leaks[81];
+  int i = 1;
+  while (i == 1 && scanf("%80[^;];%80[^;];%80[^;];%d;%f\n", plantSrc, upstreamId, downstreamId, volume, leaks) == 5) {
+    i = 0;
+    Section* firstSection = malloc(sizeof(Section));
+    strcpy(firstSection->id, upstreamId);
+    firstSection->volumeIn = volume;
+    firstSection->leaksOut = leaks;
+    firstSection->plantSrc = findAVL(plantSrc);
+    insertTree(firstSection->downstream, findAVL(downstreamId));
+    insertTree(parent(findAVL(upstreamId)), firstSection);
+    //Insert in AVL before or after maybe avoid doing findAVL by inserting in AVL first
+    /*
+    Section* up = avl_find(root, upstream);
+    if (!up) {
+        up = create_section(upstream);
+        root = avl_insert(root, up);
+    }
+    Section* down = avl_find(root, downstream);
+    if (!down) {
+        down = create_section(downstream);
+        root = avl_insert(root, down);
+    }
+    section_add_downstream(up, down);
+    section_add_upstream(down, up);
+    up->volume_max = atof(volume);
+    up->leaks = atof(leaks);
+    */
+    printf("Col1 = %s | Col2 = %s | Col3 = %s | Col4 = %d | Col5 = %f\n", plantSrc, upstreamId, downstreamId, volume, leaks);
+  }
+  return 0;
+}
