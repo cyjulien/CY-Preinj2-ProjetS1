2f37a5d0ba99a76220cadd57beccef8793ec3902
ccy946 <clement.schneider@etu.u-cergy.fr>
2025-12-19 18:41:25 +0100

    Update histo.c

--- CODE/histo/histo.c
+++ CODE/histo/histo.c
@@ -2,46 +2,112 @@
 #include "./histo.h"
 #include "./AVL.h"
 
-int compare_facility(const void *a, const void *b) {
+int compare_max(const void *a, const void *b) {
   const Facility *ua = a;
   const Facility *ub = b;
   return ua->max - ub->max;
 }
 
-int main() {
+int compare_src(const void *a, const void *b) {
+  const Facility *ua = a;
+  const Facility *ub = b;
+  return ua->src - ub->src;
+}
+
+int compare_real(const void *a, const void *b) {
+  const Facility *ua = a;
+  const Facility *ub = b;
+  return ua->real - ub->real;
+}
+
+int main(int argc, char const *argv[]) {
+
+  printf("\n %s \n ", argv[1]);
+
 
   char buf[256];
   int n = 0;
-  Facility facilities[10000];
+  int h = 0;
+  Node* root = NULL;
   while (fgets(buf, sizeof(buf), stdin)) {
-    if (n>=10000) break;
     char * facilitySrc = strtok( buf, ";" );
     char * id = strtok( NULL, ";" );
     char * aval = strtok( NULL, ";" );
-    char * max = strtok( NULL, ";" );
-    char * fuite = strtok( NULL, ";" );
+    char * max = atoi(strtok( NULL, ";" ));
+    char * fuite = atof(strtok( NULL, ";" ));
     if (n<10) printf("Reading: %s\n", id);
 
     if (!id || !max) continue;  // sécurité
 
-    strncpy(facilities[n].id, id, 31);
-    facilities[n].id[sizeof(facilities[n].id)-1] = '\0';
-    facilities[n].max = atoi(max);
 
-    n++;
-  }
-  printf("Count: %d\n", n);
 
-  qsort(facilities, n, sizeof(Facility), compare_facility);
-  int h = 0;
-  Node* root = NULL;
-  for (size_t i = 0; i < 100; i++) {
-    h = 0;
-    addChildAVL(&root, &facilities[i], &h);
+    if (max == 0 && strcmp(facilitySrc, "-") == 0) { // We check if it's a facility
+      Node* exist = searchAVL(root, id);
+      if (!exist) {
+        printf("\nData is problematic\n");
+        exit(12);
+      }
+      exist->address->max = max;
+    } else {
+      Node* node_avl = searchAVL(root, aval);
+      Facility facility = malloc(sizeof(Facility));
+      if (node_avl == NULL) {
+        facility->id = id;
+        facility->max = 0;
+        facility->src = 0;
+        facility->real = 0;
+        int h = 0;
+        addChildAVL(&root, &facility, &h);
+        n++;
+      } else facility = node_avl->address;
+
+      facility->src += max;
+      facility->real += max*fuite;
+    }
+
+    facility->src = min2(facility->src, max);
+    facility->real = facility->src >= max*fuite ? facility->src : max*fuite;
+
+    // Facility facility = malloc(sizeof(Facility));
+    // facility->id = id;
+    // facility->max = 0;
+    // facility->src = 0;
+    // facility->real = 0;
+    //
+    // int h=0;
+    // Node* existing_facility = findAVL(root, &facility);
+    // addChildAVL(&root, &facility, &h);
+
+
+    // strncpy(facilities[n].id, id, 31);
+    // facilities[n].id[sizeof(facilities[n].id)-1] = '\0';
+    // facilities[n].max = atoi(max);
+    //
+    // h = 0;
+    // addChildAVL(&root, &facilities[n], &h);
+    // n++;
+
+    //AVL d'abord (créer dans la boucle)
+    //Créer une struct Facility
+    //Check la commande
+    //Convertir en tableau
+    //En fonction de la commande trier les éléments
+
   }
+
   printTree(root, 0);
   printf("\n");
+  printf("Count: %d\n", n);
+
+  // if (argv[1] == "max") {
+  //   /* code */
+  // }
+
+  Facility facilities[n]; //Liste chainée
+  qsort(facilities, n, sizeof(Facility), compare_facility);
+
   deleteAllChilds(&root);
+
 
   // bottom 50
   FILE *csv1 = fopen("./histo/bottom50.csv", "w");
