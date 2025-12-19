efaaf3196edd567b8dea1b030b60305c657fd087
cyjulien <julien.renard@etu.cyu.fr>
2025-12-19 11:06:58 +0100

    Delete CODE directory

--- CODE/protoype_max/max.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-gcc -o test max.c
-
-grep -E '^-[;][^;]+;-[;][0-9]+;-$' c-wildwater_v3.dat | ./test
-
-for f in top10 bottom50; do
-    gnuplot -e "set datafile separator ','; set terminal pngcairo; set output '${f}.png'; plot '${f}.csv' using 2:xtic(1) with boxes"
-done
