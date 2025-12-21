618669e8b48481420d3b4f4fcd0d9df0ae5f9b79
weero_tmd <weerotmd@gmail.com>
2025-12-21 05:50:55 +0100

    Fixed histo.h

--- CODE/histo/histo.h
+++ CODE/histo/histo.h
@@ -1,1 +1,6 @@
-int compare_usine(const void *a, const void *b);
+#include "../main.h"
+int compareMax(const void *a, const void *b);
+int compareSrc(const void *a, const void *b);
+int compareReal(const void *a, const void *b);
+
+int main(int argc, char const *argv[]);
